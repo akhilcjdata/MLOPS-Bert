75529cea0cff7f836a08cbf777ff9d959127e9fb
akhilcjdata <akhilcjdata@gmail.com>
2025-03-26 17:55:30 +0530

    CI/CD 24

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -16,28 +16,28 @@ from src.logger import logging
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
 # Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+dagshub_token = os.getenv("CAPSTONE_TEST")
+if not dagshub_token:
+    raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-# dagshub_url = "https://dagshub.com"
-# repo_owner = "akhilcjdata"
-# repo_name = "YT-Capston-Project"
+dagshub_url = "https://dagshub.com"
+repo_owner = "akhilcjdata"
+repo_name = "YT-Capston-Project"
 
-# # Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 # -------------------------------------------------------------------------------------
 
 # -------------------------------------------------------------------------------------
 
 
 # Set up MLflow tracking with DAGsHub
-dagshub.init(repo_owner='akhilcjdata', repo_name='YT-Capston-Project', mlflow=True)
-mlflow.set_tracking_uri('https://dagshub.com/akhilcjdata/YT-Capston-Project.mlflow')
-mlflow.set_experiment('Transformer Embeddings Classification Models with distilbert')
+# dagshub.init(repo_owner='akhilcjdata', repo_name='YT-Capston-Project', mlflow=True)
+# mlflow.set_tracking_uri('https://dagshub.com/akhilcjdata/YT-Capston-Project.mlflow')
+# mlflow.set_experiment('Transformer Embeddings Classification Models with distilbert')
 
 def load_model(file_path: str):
     """Load the trained BERT model from a file."""
